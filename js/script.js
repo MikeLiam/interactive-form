d26bdf6a41f2945e848a3a8a882ea3aada341b71
MikeLiam <miguelalcalaromero@gmail.com>
2020-07-11 18:00:49 +0200

    onsubmit errors + color label modified

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ otherJobRole.style.display = "none";
 const jobRole = document.querySelector('select#title');
 
 jobRole.addEventListener('change', (e) => {
-    if(e.target.value === 'other') {
+    if (e.target.value === 'other') {
         otherJobRole.style.display = "inherit";
     } else {
         otherJobRole.style.display = "none";
@@ -19,6 +19,7 @@ jobRole.addEventListener('change', (e) => {
 const colorSelect = document.querySelector('select#color');
 
 colorSelect.style.display = 'none';
+colorSelect.previousElementSibling.style.visibility = 'hidden';
 
 const colorInfo = document.createElement('span');
 colorInfo.textContent = "Please select a T-shirt theme.";
@@ -29,10 +30,10 @@ const themeSelect = document.querySelector('#design');
 themeSelect.querySelector('option').hidden = true;
 
 themeSelect.addEventListener('change', (event) => {
-    function selectPackColor(list,theme) {
+    function selectPackColor(list, theme) {
         for (let i = 0; i < list.length; i++) {
-            for (let j = 0; j < theme.length; j++){
-                if (list[i].value === theme[j]){
+            for (let j = 0; j < theme.length; j++) {
+                if (list[i].value === theme[j]) {
                     list[i].hidden = false;
                     break;
                 } else {
@@ -45,10 +46,11 @@ themeSelect.addEventListener('change', (event) => {
 
     colorInfo.style.display = 'none';
     colorSelect.style.display = 'inherit';
+    colorSelect.previousElementSibling.style.visibility = 'visible';
     const colorOptions = colorSelect.querySelectorAll('option');
     const theme1Options = ["cornflowerblue", "darkslategrey", "gold"];
     const theme2Options = ["tomato", "steelblue", "dimgrey"];
-    if(event.target.value === "js puns") {
+    if (event.target.value === "js puns") {
         selectPackColor(colorOptions, theme1Options);
         colorSelect.querySelector(`option[value=${theme1Options[0]}]`).selected = true;
     } else {
@@ -67,20 +69,21 @@ activitiesField.appendChild(totalAmount);
 
 
 activitiesField.addEventListener('change', (event) => {
-    function calcTotal(sum1,sum2,operator) {
+    function calcTotal(sum1, sum2, operator) {
         let total = 0;
         if (operator === '+') {
-             total = parseInt(sum1) + parseInt(sum2);
+            total = parseInt(sum1) + parseInt(sum2);
         } else {
-             total = parseInt(sum1) - parseInt(sum2);
+            total = parseInt(sum1) - parseInt(sum2);
         }
         totalAmount.textContent = `$${total}`;
         totalAmount.dataset.value = total;
     }
-    function toggleOffOnActivities(activity,option) {
+
+    function toggleOffOnActivities(activity, option) {
         for (let i = 0; i < activities.length; i++) {
             if (activities[i].dataset.dayAndTime === activity.dataset.dayAndTime && activities[i] !== activity) {
-                    activities[i].disabled = option;
+                activities[i].disabled = option;
             }
         }
     }
@@ -91,6 +94,7 @@ activitiesField.addEventListener('change', (event) => {
         if (event.target.checked === true) {
             operator = '+'
             toggleOffOnActivities(event.target, true);
+            showInfo('', document.querySelector('span.js-activities-span'));
         } else {
             operator = '-';
             toggleOffOnActivities(event.target, false);
@@ -104,7 +108,7 @@ paymentSelect.querySelector('option[value="select method"]').hidden = true;
 paymentSelect.querySelector('option[value="credit card"]').selected = true;
 selectPayment("credit card");
 
-function selectPayment(payment){
+function selectPayment(payment) {
     const paymentField = paymentSelect.parentNode;
     const creditCard = paymentField.querySelector('#credit-card');
     const paypal = paymentField.querySelector('#paypal');
@@ -114,7 +118,7 @@ function selectPayment(payment){
         paypal.style.display = "none";
         bitcoin.style.display = "none";
         creditCard.style.display = "inherit";
-    } else{
+    } else {
         paymentField.querySelector('#credit-card').style.display = "none";
         if (payment === "paypal") {
             bitcoin.style.display = "none";
@@ -123,13 +127,12 @@ function selectPayment(payment){
             paypal.style.display = "none";
             bitcoin.style.display = "inherit";
         }
-    } 
+    }
 }
 
 paymentSelect.addEventListener('change', (event) => {
     selectPayment(event.target.value);
 });
-
 
 
 const nameInput = document.querySelector('input#name');
@@ -138,53 +141,121 @@ const creditNumber = document.querySelector('input#cc-num');
 const creditZip = document.querySelector('input#zip');
 const creditCvv = document.querySelector('input#cvv');
 
-function createSpan(text, inputElement) {
+function createSpan(typeSpan, inputElement) {
     const element = document.createElement('span');
-    element.textContent = text;
+    element.className = typeSpan;
     element.style.display = 'none';
     inputElement.parentNode.insertBefore(element, inputElement.nextElementSibling);
 }
 
-createSpan("Name field can't be blank.",nameInput);
-createSpan("Email field must be a validly formatted e-mail address like email@email.com or email@email.org.com",emailInput);
-createSpan("Credit Card field should be number between 13 and 16 digits.",creditNumber);
-createSpan("The Zip Code field should be a 5-digit number.",creditZip);
-createSpan("The CVV should be a 3-digit number.",creditCvv);
-createSpan("At least one actvitiy should be selected.",activitiesField.firstElementChild);
+createSpan('js-info-span', nameInput);
+createSpan('js-info-span', emailInput);
+createSpan('js-info-span', creditNumber);
+createSpan('js-info-span', creditZip);
+createSpan('js-info-span', creditCvv);
+createSpan('js-activities-span', activitiesField.firstElementChild);
 
+function validation(forTest, tester) {
+    return tester.test(forTest) && forTest != '';
+}
 
-function showOrHideTip(show, element) {
-    if (show) {
-        element.style.display = 'inherit';
-    } else {
+function showInfo(message, element) {
+    console.log(element);
+    if (message === '') {
         element.style.display = 'none';
+        element.previousElementSibling.style.border = 'green solid';
+        element.classList.remove('error');
+    } else {
+        element.style.display = 'inherit';
+        element.classList.add('error');
+        element.textContent = message;
+        element.previousElementSibling.style.border = 'red solid';
+
     }
 }
 
-function validation(tester, forTest) {
-    return tester.test(forTest);
-}
-
-
-function createListener(reference) {
+function createListener(reference, label, message) {
     return event => {
         let input;
-        if (event.target === activities) {
-            input = activities.querySelector('#total').dataset.value;
+        let element = event.target.nextElementSibling;
+
+        if (event.target.parentNode.parentNode === activitiesField) {
+            input = activitiesField.querySelector('#total').dataset.value;
+            console.log(input);
+            element = activitiesField.querySelector('span.js-info-span');
+            console.log(element);
         } else {
             input = event.target.value;
         }
-        const valid = validation(reference, input);
-        const showTip = !valid;
-        const toolTip = event.target.nextElementSibling;
-        showOrHideTip(showTip, toolTip);
+
+        const valid = validation(input, reference);
+
+        if (input.length === 0) {
+            showInfo(`Please enter a ${label}.`, element);
+        } else if (!valid) {
+            console.log(event.target);
+            showInfo(message, element);
+        } else {
+            showInfo('', element);
+        }
     };
 }
 
-nameInput.addEventListener('input', createListener(/^[a-z]{1,}$/i));
-emailInput.addEventListener('input', createListener(/^[^@]+\@[^@]+(\.[a-z]{3})?\.[a-z]{3}?$/i));
-creditNumber.addEventListener('input', createListener(/^\d{13,16}$/));
-creditZip.addEventListener('input', createListener(/^\d{5}$/));
-creditCvv.addEventListener('input', createListener(/^\d{3}$/));
-activitiesField.addEventListener('input', createListener(/^\$\d{1,}$/));
+nameInput.addEventListener('input', createListener(/^[a-z]{1,}$/i, 'name', "Name field can't be blank."));
+emailInput.addEventListener('input', createListener(/^[^@]+\@[^@]+(\.[a-z]{3})?\.[a-z]{3}?$/i, 'e-mail address', 
+"Email field must be a validly formatted e-mail address like email@email.com or email@email.org.com"));
+creditNumber.addEventListener('input', createListener(/^\d{13,16}$/, 'credit card number',
+"Credit Card should be number between 13 and 16 digits." ));
+creditZip.addEventListener('input', createListener(/^\d{5}$/, 'Zip Code number', 
+"The Zip Code should be a 5-digit number."));
+creditCvv.addEventListener('input', createListener(/^\d{3}$/, 'CVV number', 
+"The CVV number should be a 3-digit number."));
 
+const form = document.querySelector('form');
+
+
+function validateErrors() {
+    function validateActivities() {
+
+        for (let i = 0; i < activities.length; i++) {
+            if (activities[i].checked) {
+                showInfo('', 
+                document.querySelector('span.js-activities-span'))
+                return true;
+            } else {
+                showInfo('Please at least one activity should be seleceted',
+                document.querySelector('span.js-activities-span'));
+            }
+        }
+
+        return false;
+    }
+
+    const spanElements = document.querySelectorAll('span.js-info-span');
+    let noError = true;
+
+    for (let i = 0; i < spanElements.length; i++) {
+        if (spanElements[i].style.display === 'inherit') {
+            noError = false;
+        } else if (spanElements[i].previousElementSibling.value === '') {
+                showInfo('Please fill this field', spanElements[i]);
+                console.log(spanElements[i]);
+                noError = false;
+        }
+    }
+    noError = validateActivities() && noError;
+    if (!noError){
+        document.querySelector('span.error').previousElementSibling.focus();
+    }
+    return noError;
+}
+
+form.addEventListener('submit', (e) => {
+
+    if (validateErrors()) {
+        // Submit form
+    } else {
+        e.preventDefault();
+    }
+
+});
