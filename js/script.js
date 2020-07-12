f76e63d670e71bf92eece427160f6b8f4b58fca8
MikeLiam <miguelalcalaromero@gmail.com>
2020-07-12 12:20:28 +0200

    Comments added

--- js/script.js
+++ js/script.js
@@ -17,26 +17,30 @@ jobRole.addEventListener('change', (e) => {
     }
 });
 
+// Hide color selection and color label till one theme is selected
 const colorSelect = document.querySelector('select#color');
-
 colorSelect.style.display = 'none';
 colorSelect.previousElementSibling.style.visibility = 'hidden';
-
-const colorInfo = document.createElement('span');
+// And show info
+const colorInfo = document.createElement('p');
 colorInfo.textContent = "Please select a T-shirt theme.";
-
 colorSelect.parentNode.appendChild(colorInfo);
 
+// Theme selection 'Select a theme' disabling 
 const themeSelect = document.querySelector('#design');
 themeSelect.firstElementChild.disabled = true;
+// and create a copy of color options for reasson below
 const colorOptions = [...colorSelect.querySelectorAll('option')];
-
-
+// Listener for change at theme selection
 themeSelect.addEventListener('change', (event) => {
+    // For crossbrowser compatibility (safari doesn't support atribute hidden) 
+    // remove and append colors as a theme is selected
     function selectPackColor(list, theme) {
+        // First remove all colors
         while (colorSelect.firstElementChild){
             colorSelect.removeChild(colorSelect.firstElementChild);
         }
+        // Then append theme's colors
         for(let i = 0; i < list.length; i++){
             for (let j = 0; j < theme.length; j++) {
                 if (list[i].value === theme[j]) {
@@ -46,14 +50,14 @@ themeSelect.addEventListener('change', (event) => {
             }
         }
     }
-
+    // At a theme selected, show color selection and color label hidding info
     colorInfo.style.display = 'none';
     colorSelect.style.display = 'inherit';
     colorSelect.previousElementSibling.style.visibility = 'visible';
-
+    // colors availables for each theme option
     const theme1Options = ["cornflowerblue", "darkslategrey", "gold"];
     const theme2Options = ["tomato", "steelblue", "dimgrey"];
-
+    // Selection
     if (event.target.value === "js puns") {
         selectPackColor(colorOptions, theme1Options);
         colorSelect.querySelector(`option[value=${theme1Options[0]}]`).selected = true;
@@ -63,16 +67,25 @@ themeSelect.addEventListener('change', (event) => {
     }
 });
 
+// Activities field
 const activitiesField = document.querySelector('.activities div');
 const activities = activitiesField.querySelectorAll('input');
+// Create and append a total cost for activities selected
 const totalAmount = document.createElement('p');
 totalAmount.id = 'total';
 totalAmount.textContent = '$0';
 totalAmount.dataset.value = 0;
 activitiesField.appendChild(totalAmount);
 
-
+// Listener for a change at activities selected
 activitiesField.addEventListener('change', (event) => {
+    /**
+     * Calculate new total cost afer add (operator +) a new activity cost (sum2)
+     * or substract (operator -) to current total (sum1)
+     * @param {string} sum1 
+     * @param {string} sum2 
+     * @param {string} operator 
+     */
     function calcTotal(sum1, sum2, operator) {
         let total = 0;
         if (operator === '+') {
@@ -83,7 +96,12 @@ activitiesField.addEventListener('change', (event) => {
         totalAmount.textContent = `$${total}`;
         totalAmount.dataset.value = total;
     }
-
+    /**
+     * Toggle disabled property at (option) value of other activities if 
+     * have same time/day that a selected/deselected (activity)
+     * @param {HTML element} activity 
+     * @param {Boolean} option 
+     */
     function toggleOffOnActivities(activity, option) {
         for (let i = 0; i < activities.length; i++) {
             if (activities[i].dataset.dayAndTime === activity.dataset.dayAndTime && activities[i] !== activity) {
@@ -91,15 +109,15 @@ activitiesField.addEventListener('change', (event) => {
             }
         }
     }
-
+    // When a input type checkbox is clicked
     if (event.target.type === "checkbox") {
         let operator;
-
+        // If it is for check, operator for add cost and watch if disable other activity at same time/day
         if (event.target.checked === true) {
             operator = '+'
             toggleOffOnActivities(event.target, true);
             showInfo('', document.querySelector('span.js-activities-span'));
-        } else {
+        } else { // operator for substract cost and watch if may undo disable other actvity at same time/day
             operator = '-';
             toggleOffOnActivities(event.target, false);
         }
@@ -107,17 +125,22 @@ activitiesField.addEventListener('change', (event) => {
     }
 });
 
+// Payment field
 const paymentSelect = document.querySelector('select#payment');
-paymentSelect.querySelector('option[value="select method"]').hidden = true;
+// Disable "select method" option
+paymentSelect.querySelector('option[value="select method"]').disabled = true;
 paymentSelect.querySelector('option[value="credit card"]').selected = true;
-selectPayment("credit card");
 
+/**
+ * Event Handler to show payment selected and hide others
+ * @param {String} payment 
+ */
 function selectPayment(payment) {
     const paymentField = paymentSelect.parentNode;
     const creditCard = paymentField.querySelector('#credit-card');
     const paypal = paymentField.querySelector('#paypal');
     const bitcoin = paymentField.querySelector('#bitcoin');
-
+    
     if (payment === "credit card") {
         paypal.style.display = "none";
         bitcoin.style.display = "none";
@@ -133,42 +156,59 @@ function selectPayment(payment) {
         }
     }
 }
-
+// By default "credit card" payment is selected
+selectPayment("credit card");
+// Listener for payment selection
 paymentSelect.addEventListener('change', (event) => {
     selectPayment(event.target.value);
 });
 
-
+// Inputs for validation
 const nameInput = document.querySelector('input#name');
 const emailInput = document.querySelector('input#mail');
 const creditNumber = document.querySelector('input#cc-num');
 const creditZip = document.querySelector('input#zip');
 const creditCvv = document.querySelector('input#cvv');
 
-function createSpan(typeSpan, inputElement) {
+/**
+ * Create span to show messages at inputs validation or submit form
+ * @param {String} typeSpan 
+ * @param {HTML element} inputElement 
+ */
+function createSpan(classSpan, inputElement) {
     const element = document.createElement('span');
-    element.className = typeSpan;
+    element.className = classSpan;
     element.style.display = 'none';
     inputElement.parentNode.appendChild(element);
 }
-
+// One span for each field to validate
 createSpan('js-info-span', nameInput);
 createSpan('js-info-span', emailInput);
 createSpan('js-info-span', creditNumber);
 createSpan('js-info-span', creditZip);
 createSpan('js-info-span', creditCvv);
 createSpan('js-activities-span', activitiesField.querySelector('p#total'));
-
+/**
+ * General method for validate an input (forTest) with a given regular expression (tester)
+ * @param {String} forTest 
+ * @param {RegEx} tester 
+ */
 function validation(forTest, tester) {
     return tester.test(forTest) && forTest != '';
 }
 
+/**
+ * Toggle visibility of span element and warning color for input elements. 
+ * Class error let us identificate active span element.
+ * @param {String} message 
+ * @param {HTML element} element 
+ */
 function showInfo(message, element) {
     console.log(element);
     if (message === '') {
         element.style.display = 'none';
         if (element.previousElementSibling.tagName === 'INPUT') {
-            element.previousElementSibling.style.border = 'green solid';
+            element.previousElementSibling.style.border = 'darkgreen solid 0.1em';
         }
         element.classList.remove('error');
     } else {
@@ -176,11 +216,19 @@ function showInfo(message, element) {
         element.classList.add('error');
         element.textContent = message;
         if (element.previousElementSibling.tagName === 'INPUT') {
-            element.previousElementSibling.style.border = 'red solid';
+            element.previousElementSibling.style.border = '#ac313b solid 0.2em';
         }
     }
 }
-
+/**
+ * Create an event handler for each input to validate it with a Regular Expression (reference)\
+ * and show info warning user if needed:
+ * basic warning at blanck input to enter info with (label) 
+ * advanced warning with info needed with (message)
+ * @param {RegEx} reference 
+ * @param {String} label 
+ * @param {String} message 
+ */
 function createListener(reference, label, message) {
     return event => {
         let input;
@@ -201,6 +249,7 @@ function createListener(reference, label, message) {
     };
 }
 
+// Input listeners
 nameInput.addEventListener('input', createListener(/^[a-z]{1,}$/i, 'name', "Only a-z letters permited"));
 emailInput.addEventListener('input', createListener(/^[^@]+\@[^@]+(\.[a-z]{3})?\.[a-z]{3}?$/i, 'e-mail address', 
 "Should be a validly formatted e-mail address"));
@@ -211,10 +260,16 @@ creditZip.addEventListener('input', createListener(/^\d{5}$/, 'Zip Code number',
 creditCvv.addEventListener('input', createListener(/^\d{3}$/, 'CVV number', 
 "The CVV number should be a 3-digit number."));
 
+// At submit form validation
 const form = document.querySelector('form');
 
-
+/**
+ * Run test to know if there is some warning shown, some empty input or none activity selected.
+ */
 function validateErrors() {
+    /**
+     * Activity test module
+     */
     function validateActivities() {
 
         for (let i = 0; i < activities.length; i++) {
@@ -235,25 +290,29 @@ function validateErrors() {
     let noError = true;
 
     for (let i = 0; i < spanElements.length; i++) {
+        // Warnings shown test module
         if (spanElements[i].style.display === 'inherit') {
             noError = false;
         } else if (spanElements[i].previousElementSibling.value === '') {
+            // Empty inputs test module
                 showInfo('Please fill this field', spanElements[i]);
                 console.log(spanElements[i]);
                 noError = false;
         }
     }
     noError = validateActivities() && noError;
+    // Focus at first input with a warning
     if (!noError){
         document.querySelector('span.error').previousElementSibling.focus();
     }
     return noError;
 }
 
+// Listener for submit form
 form.addEventListener('submit', (e) => {
 
     if (validateErrors()) {
-        // Submit form
+        // Send form inormation
     } else {
         e.preventDefault();
     }
